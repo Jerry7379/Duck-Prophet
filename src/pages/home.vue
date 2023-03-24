f260f6ec7556e8d7a847c747df12d55930a21575
Jerry7379 <407847057@qq.com>
2023-03-24 16:04:28 +0800

    underLine

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>{{ text }}</div>
+    <div>
+        <h2 class="title"><span>{{ text }}</span></h2>
+    </div>
 </template>
 <script lang="ts">
 export default {
     data() {
         return {
-            text: "sssssssssssssssss"
+            text: "搞点不一样的下划线"
         }
     },
     methods: {
-        
+
     }
 }
 </script>
+<style scoped>
+.title {
+    color: #333;
+    line-height: 2;
+}
+
+.title span {
+    background: linear-gradient(to right, #fff, #000) no-repeat left bottom;
+    background-size: 0 2px;
+    transition: background-size 1300ms;
+}
+
+.title span:hover {
+    background-position-x: left;
+    background-size: 100% 2px;
+}
+</style>
