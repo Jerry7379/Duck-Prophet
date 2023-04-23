7652b556b18e4d402688b52770ed3e89334ce195
Jerry7379 <407847057@qq.com>
2023-04-23 23:11:49 +0800

    加些css

---
+++ src/components/CssRotatingBall.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="box"></div>
+</template>
+<script lang="ts">
+export default {
+    name: "cssRotaingBall",
+    data() {
+        return {
+            text: "搞点不一样的下划线"
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style scoped>
+.box {
+    width: 200px;
+    height: 200px;
+    background: transparent;
+    /* 背景透明 */
+    border: 5px solid #00d9ff;
+    border-radius: 50%;
+    position: relative;
+    animation: round 2s infinite;
+}
+
+.box:before {
+    position: absolute;
+    top: 15px;
+    left: 10px;
+    content: "";
+    width: 30px;
+    height: 30px;
+    background: rgb(255, 132, 0);
+    border-radius: 50%
+}
+
+@keyframes round {
+    0% {
+        rotate: 0deg;
+    }
+
+    100% {
+        rotate: 360deg;
+    }
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,18 +3,36 @@
         <h2>css相关的一些奇技淫巧</h2>
         <h2 class="title"><span>{{ text }}</span></h2>
         <img class="auto-head" src="../assets/head2.png" alt="头像">
+        <CssRotatingBall></CssRotatingBall>
+
+        <div class="btn">
+            鼠标移入展示窗口
+            <div class="detail">
+                <div class="content">
+                    <div class="inner">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam voluptatum eaque ex
+                        aperiam
+                        saepe! Veniam quaerat adipisci quia. Tempore sit eaque placeat, mollitia nam impedit dolores
+                        perferendis
+                        quos
+                        quisquam velit?
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script lang="ts">
+import CssRotatingBall from "../components/CssRotatingBall.vue";
+
 export default {
     data() {
         return {
             text: "搞点不一样的下划线"
-        }
+        };
     },
-    methods: {
-
-    }
+    methods: {},
+    components: { CssRotatingBall }
 }
 </script>
 <style scoped>
@@ -47,7 +65,7 @@ export default {
     height: var(--s);
     cursor: pointer;
     transition: scale(var(--f));
-    padding-top: 5px;
+    padding-top: 50px;
     outline: var(--b) solid var(--c1);
     outline-offset: var(--shrink);
     border-radius: 0 0 999px 999px;
@@ -58,13 +76,33 @@ export default {
             var(--c1) 99%,
             transparent 100%) var(--bgOption);
     -webkit-mask:
-        linear-gradient(#000 0 0) 
-        no-repeat center 
-        calc(1px - var(--shrink)) / calc(100% / var(--f) - 2 * var(--b) - 1px) 50%,
+        linear-gradient(#000 0 0) no-repeat center calc(1px - var(--shrink)) / calc(100% / var(--f) - 2 * var(--b) - 1px) 50%,
         radial-gradient(circle closest-side, #000 99%, transparent) var(--bgOption);
 }
 
 .auto-head:hover {
     --f: 1.35
+}
+
+.content {
+    background-color: #222;
+    color: #fff;
+    line-height: 1.5;
+    min-height: 0;
+}
+
+.inner {
+    padding: 20px;
+}
+
+.detail {
+    display: grid;
+    grid-template-rows: 0fr;
+    transition: .5s;
+
+}
+
+.btn:hover .detail {
+    grid-template-rows: 1fr;
 }
 </style>
